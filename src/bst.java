02964f87ff319edb85d430497afd421ea1456ddf
Jared Powell <jared.powell261@topper.wku.edu>
2018-10-28 09:59:25 -0500

    Added AVL Tree and find compares number of nodes traversed TODO: Make max and min compare to AVL tree, ensure my AVL tree is okay to use from website

--- src/bst.java
+++ src/bst.java
@@ -1,57 +1,72 @@
 public class bst {
+
     node root;
+    int count; // Counts number of nodes traversed in search
 
-   public bst() {
-       this.root = null;
-   }
+    public bst() {
+        this.root = null;
+        this.count = 0;
+    }
 
-   public void insert(student s) {
-       if (root == null) {
-           root = new node(s);
-           return;
-       }
-       node parent = null;
-       node current = root;
-       while (true) {
-           parent = current;
-           if(current.s.compareTo(s) < 0) {
-               current = current.right;
-               if(current == null) {
-                   parent.right = new node(s);
-                   return;
-               }
-           } else {
-               current = current.left;
-               if(current == null) {
-                   parent.left = new node(s);
-                   return;
-               }
-           }
+    public void insert(student s) {
+        if (root == null) {
+            root = new node(s);
+            return;
         }
-   }
-
-   public student find(int id) {
-       node current = root;
-       while(current != null) {
-           if(current.s.id == id)
-               return current.s;
-           else if (current.s.id >= id)
-               current = current.left;
-           else
-               current = current.right;
-       }
-       return null;
-   }
-   public student findMinID() {
-       node current = root;
-       while(current.hasLeft())
-        current = current.left;
-       return current.s;
-   }
-    public student findMaxID() {
+        node parent = null;
         node current = root;
-        while(current.hasRight())
-            current = current.right;
+        while (true) {
+            parent = current;
+            if (current.s.compareTo(s) < 0) {
+                current = current.right;
+                if (current == null) {
+                    parent.right = new node(s);
+                    return;
+                }
+            } else {
+                current = current.left;
+                if (current == null) {
+                    parent.left = new node(s);
+                    return;
+                }
+            }
+        }
+    }
+
+    public student find(int id) {
+        node current = root;
+        while (current != null) {
+            count++;
+            if (current.s.id == id) {
+                return current.s;
+            } else if (current.s.id >= id) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return null;
+    }
+
+    public student findMinID() {
+        node current = root;
+        while (current.hasLeft()) {
+            current = current.left;
+        }
         return current.s;
     }
+
+    public student findMaxID() {
+        node current = root;
+        while (current.hasRight()) {
+            current = current.right;
+        }
+        return current.s;
+    }
+     public int displayCount() {
+         // return count value and reset it for future use
+         int temp = this.count;
+         this.count = 0;
+         return temp;
+     }
 }
--- src/cs421project1.java
+++ src/cs421project1.java
@@ -9,42 +9,40 @@ public class cs421project1 {
     public static void main(String[] args) throws FileNotFoundException {
 
         bst tree = new bst();
+        AVLTree avl = new AVLTree();
         File file = new File("test1.txt");
         int id;
         String name;
         double gpa;
 
-        // Create File reader
-        Scanner reader = new Scanner(file);
         // Get line from reader
-        String line = reader.nextLine();
-        int size = Integer.parseInt(line);
-        // Will go through all but the last line to parse data
-        while(reader.hasNext()) {
-            line = reader.next();
-            id = Integer.parseInt(line);
-            name = reader.next();
-            line = reader.next();
-            gpa = Double.parseDouble(line);
-            student newStudent = new student(id, name, gpa);
-            // newStudent.print();
-            tree.insert(newStudent);
+        try ( // Create File reader
+                Scanner reader = new Scanner(file)) {
+            // Get line from reader
+            String line = reader.nextLine();
+            int size = Integer.parseInt(line);
+            // Will go through all but the last line to parse data
+            while (reader.hasNext()) {
+                line = reader.next();
+                id = Integer.parseInt(line);
+                name = reader.next();
+                line = reader.next();
+                gpa = Double.parseDouble(line);
+                student newStudent = new student(id, name, gpa);
+                // newStudent.print();
+                tree.insert(newStudent);
+                avl.insert(newStudent);
+            }
         }
-        reader.close();
 
         //TODO: AVL tree (I believe in you Stephen
         // System.out.println(tree.find(1));
-
         String choice = " ";
         Scanner input = new Scanner(System.in);
 
         //Menu with 3 options
         //Compares strings so we won't have to deal with as many error exceptions
-
-        //****As of right now it prints all options twice on each iteration***
-
-
-        while(choice != "exit"){
+        while (!choice.equals("exit")) {
             System.out.println("1) Search for student by ID");
             System.out.println("2) Find student info with lowest ID");
             System.out.println("3) Find student info with greatest ID");
@@ -52,36 +50,43 @@ public class cs421project1 {
 
             choice = input.nextLine();
 
-            switch (choice){
+            switch (choice) {
 
                 //Find Option
-                case "1": System.out.println("What student id do you want to find? ");
+                case "1":
+                    System.out.println("What student id do you want to find? ");
                     int idSearch = input.nextInt();
-                    student foundStudent = tree.find(idSearch);
-                    if(foundStudent != null){
-                        System.out.println(foundStudent);
-                    }
-                    else {
+                    if (avl.search(idSearch)) {
+                        student foundStudent = tree.find(idSearch);
+                        if (foundStudent != null) {
+                            System.out.println(foundStudent);
+                            System.out.println("Nodes BST tree traversed: " + tree.displayCount());
+                            System.out.println("Nodes AVL tree traversed: " + avl.displayCount());
+                        }
+                    } else {
                         System.out.println("Could not find Student with id: " + idSearch);
                     }
+
                     input.nextLine();
                     break;
 
                 //Min Option
-                case "2": student testStudent = tree.findMinID();
+                case "2":
+                    student testStudent = tree.findMinID();
                     System.out.print("Min ID Student: " + testStudent);
                     break;
 
-
                 //Max Option
-                case "3": student testStudent1 = tree.findMaxID();
+                case "3":
+                    student testStudent1 = tree.findMaxID();
                     System.out.print("Max ID Student: " + testStudent1);
                     break;
                 case "exit":
                     System.out.println("Good-bye");
-                    return;
+                    break;
 
-                default : System.out.println("Invalid Choice. Please Try Again \n");
+                default:
+                    System.out.println("Invalid Choice. Please Try Again \n");
             }
 
         }
